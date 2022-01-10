79c07483fd0c60e4dd5588083bd0a9d6455f34c1
Aleksey Bobrovskyi <leshabobrovsky99@gmail.com>
2022-01-10 23:36:19 +0200

    add

---
+++ src/Api/Api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const key = '24499282-2c88a00bf4afff93913de8ef0';
+
+const instance = axios.create({
+  baseURL: 'https://pixabay.com/api/',
+  params: {
+    key: key,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: 12,
+  },
+});
+
+const fetchImages = ({page, query}) => {
+  return instance.get('/', {
+    params: {
+      page,
+      q: query
+    },
+  })
+  .then(response => response.data);
+};
+
+export const Api = {
+  fetchImages,
+};
---
+++ src/components/App.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import Gallery from "./Gallery/Gallery";
+import SearchBar from "./SearchBar/SearchBar";
+import Modal from "./Modal/Modal";
+import Button from "./Button/Button";
+import { Api } from "../Api/Api";
+import "../../src/App.css";
+import styles from "../components/Modal/Modal.module.css";
+import Loader from "react-loader-spinner";
+
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [largeImageURL, setLargeImageURL] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    console.log('searchQuery :>> ', searchQuery);
+    console.log('currentPage :>> ', currentPage);
+    if (!searchQuery) return;
+    console.log("after if");
+    const onSearch = async () => {
+      setIsLoading(true);
+      try {
+        const data = await Api.fetchImages({searchQuery, currentPage});
+        //if(data?.hits.length < 11){return null;}
+        console.log("object", data);
+        if (!data.hits.length) {
+          throw new Error("Sorry we can't find anything")
+        }
+        if (data?.hits.length > 11) {
+          setImages(prevImages => [...prevImages, ...data.hits]);
+          console.log(data);
+          setIsLoading(false);
+        }
+        //setCurrentPage((prevPage) => prevPage + 1);
+
+      } catch (error) {
+        setError(error.message);
+        // console.log(error);
+        // const result = await Api.fetchImages(currentPage, searchQuery);
+        // console.log(result)
+
+      } finally {
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: "smooth",
+        });
+        setIsLoading(false);
+      }
+    };
+    console.log("after Fn");
+    onSearch();
+  }, [searchQuery, currentPage]);
+
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const getLargeUrl = (url) => {
+    setLargeImageURL(url);
+    setIsModalOpen(true);
+  };
+
+  const onFormSubmit = (item) => {
+    setSearchQuery(item);
+    setImages([]);
+    setCurrentPage(1);
+  };
+
+  const onLoadMore = () => {
+    setIsLoading(true);
+    setCurrentPage((prevPage) => prevPage + 1);
+  }
+
+  //render() {
+  //const { isModalOpen, images, largeImageURL, isLoading } = this.state;
+  //console.log(this.state.searchQuery)
+
+  return (
+    <div className="App">
+      <SearchBar onSubmit={onFormSubmit} />
+      {error && <p>{error.message}</p>}
+      <Gallery images={images} onClick={getLargeUrl} />
+      {isModalOpen && (
+        <Modal onClose={toggleModal}>
+          <img className={styles.Modal} src={largeImageURL} alt="" />
+        </Modal>
+      )}
+      {isLoading && <Loader />}
+      {images.length >= 12 && !isLoading &&
+        <Button onClick={onLoadMore} />
+      }
+    </div>
+  );
+}
+
+export default App;
